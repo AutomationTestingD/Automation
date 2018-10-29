7ed621ecf2c3ca68f0644836bf01321a857e9a09
Dileep250 <44161109+Dileep250@users.noreply.github.com>
2018-10-29 12:55:44 +0530

    Java Strings

--- src/firstpackage/DuplicateStrings.java
+++ src/firstpackage/DuplicateStrings.java
@@ -1,42 +1,49 @@
-package firstpackage;
+package jAVASTRINGS;
+
 
 import java.util.HashMap;
+
 import java.util.Iterator;
 
-public class DuplicateStrings {
-	
-	
-	
-	
-	public static void main(String[] args) {
-		
-		findDuplicateWord("I am am a Java lerner and and Selenium Selenium lerner lerner lerner");
-	}
 
-	private static void findDuplicateWord(String string) {
+
+public class DuplicateStrings {			
+
+		public static void main(String[] args) {
 		
-		String[] str = string.split(" ");
+			findDuplicateWord("I am am a Java lerner and and Selenium Selenium lerner lerner lerner");
+
+		   }
+
+		private static void findDuplicateWord(String string) {
 		
-		HashMap<String, Integer> Hm = new HashMap<>();
+			String[] str = string.split(" ");
 		
-		for(String tempstring : str) {
+			HashMap<String, Integer> Hm = new HashMap<>();
+		
+			for(String tempstring : str) {
 			
-			 if(Hm.get(tempstring) != null) {
-				Hm.put(tempstring, Hm.get(tempstring) + 1);
-			    }
-			else
-			    Hm.put(tempstring, 1);}
-								
-		Iterator<String> itrstr = Hm.keySet().iterator();
-		System.out.println(Hm);
-			
-			while (itrstr.hasNext()) {
+				 if(Hm.get(tempstring) != null) {
+
+					Hm.put(tempstring, Hm.get(tempstring) + 1);
+
+				    }
+
+				else
+
+				    Hm.put(tempstring, 1);}
+							
+			Iterator<String> itrstr = Hm.keySet().iterator();
+
+			System.out.println(Hm);
+		
+				while (itrstr.hasNext()) {
 				
-				String temp = itrstr.next();
-				if(Hm.get(temp) > 1) {
-				System.out.println("'"+temp+"' Word is appeared "+Hm.get(temp)+ " times ");			}}
+					String temp = itrstr.next();
+					if(Hm.get(temp) > 1) {
+					System.out.println("'"+temp+"' Word is appeared "+Hm.get(temp)+ " times ");			}}
+			
 					
-						
-				}
+					}
 
-}
+	}
